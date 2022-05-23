b245491bbeb079891b2455e47578b1d5ccae8604
Woong Ze Yi <woongzeyi@gmail.com>
2022-05-23 06:59:33 +0000

    Patch code inconsistency

--- ex1-3.cpp
+++ ex1-3.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-int main(void) {
 
+int main(void) {
     printf("Hello world!\n");
     cout << "Hello Selena, ";
     cout << "my height is " << 1 << "m " << 69 << "cm.";
